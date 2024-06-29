b852d094c203b825ffd257601857a79d9884762a
Rick South <rick@natamus.com>
2024-06-29 10:40:00 +0200

    (v1.2) Fixes a desync issue between client and server while on parkour blocks. [+1]

--- Common/src/main/java/com/natamus/letsparkour/mixin/ServerGamePacketListenerImplMixin.java
+++ Common/src/main/java/com/natamus/letsparkour/mixin/ServerGamePacketListenerImplMixin.java
@@ -2,31 +2,23 @@ package com.natamus.letsparkour.mixin;
 
 import com.natamus.letsparkour.block.base.ParkourSlab;
 import net.minecraft.core.BlockPos;
-import net.minecraft.network.protocol.game.ServerboundMovePlayerPacket;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.server.network.ServerGamePacketListenerImpl;
 import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.Slice;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(value = ServerGamePacketListenerImpl.class, priority = 1001)
 public abstract class ServerGamePacketListenerImplMixin {
 	@Shadow public ServerPlayer player;
 
-//	@Inject(method = "handleMovePlayer(Lnet/minecraft/network/protocol/game/ServerboundMovePlayerPacket;)V",
-//			at = @At(value = "INVOKE", target = "Lnet/minecraft/server/network/ServerGamePacketListenerImpl;isSingleplayerOwner()Z"),
-//			cancellable = true)
 	@ModifyVariable(
 			method = "handleMovePlayer(Lnet/minecraft/network/protocol/game/ServerboundMovePlayerPacket;)V",
 			at = @At("STORE"),
-			slice = @Slice(
-					from = @At(value = "INVOKE", target = "Lnet/minecraft/server/level/ServerPlayer;isFallFlying()Z")
-			),
+			slice = @Slice(from = @At(value = "INVOKE", target = "Lnet/minecraft/server/level/ServerPlayer;isFallFlying()Z")),
 			ordinal = 0
 	)
 	public boolean overrideMovementCheckIfParkouring(boolean isFallFlying) {
