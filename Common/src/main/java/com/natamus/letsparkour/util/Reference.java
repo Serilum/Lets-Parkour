3c7ca8c329018de2a31d7222f3d6d33201d4c878
Rick South <rick@natamus.com>
2026-03-26 01:43:13 +0100

    Let's Parkour source code for 26.1.0.

---
+++ Common/src/main/java/com/natamus/letsparkour/util/Reference.java
@@ -0,0 +1,8 @@
+package com.natamus.letsparkour.util;
+
+public class Reference {
+	public static final String MOD_ID = "letsparkour";
+	public static final String NAME = "Let's Parkour";
+	public static final String VERSION = "2.2";
+	public static final String ACCEPTED_VERSIONS = "[26.1.0]";
+}
